4b24f7de79923370304a46823ea83abc0d87339f
nadchhe <152775954+nadchhe@users.noreply.github.com>
2024-02-28 15:16:57 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const rdata= [{
+id:1,
+title: 'car',
+price: 100,
+company: "mini-copper",
+info: "my dream car",
+inCart: false,
+count: 1
+
+},
+{
+id:2,
+title:'car',
+price:50,
+company: "BM",
+info: "a nice car",
+inCart :false,
+count: 1
+
+
+} ]
